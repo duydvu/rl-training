6a9cdc269f5a03243374780dff9886fb0fb1c14f
Vũ Đức Duy <duyisking1060@gmail.com>
2020-08-05 14:56:42 +0000

    Update Memory

--- Memory.py
+++ Memory.py
@@ -9,54 +9,41 @@ class Memory:
     def __init__(
             self,
             capacity,
-            length = None,
-            states = None,
-            actions = None,
-            rewards = None,
-            dones = None,
-            states2 = None,       
     ):
         self.capacity = capacity
         self.length = 0
-        self.states = states
-        self.actions = actions
-        self.rewards = rewards
-        self.dones = dones
-        self.states2 = states2
+        self.states = []
+        self.actions = []
+        self.rewards = []
+        self.dones = []
+        self.states2 = []
 
     def push(self, s, a, r, done, s2):
-        if self.states is None:
-            self.states = s
-            self.actions = a
-            self.rewards = r
-            self.dones = done
-            self.states2 = s2
-        else:
-            self.states = np.vstack((self.states,s))
-            self.actions = np.vstack((self.actions,a))
-            self.rewards = np.vstack((self.rewards, r))
-            self.dones = np.vstack((self.dones, done))
-            self.states2 = np.vstack((self.states2,s2))
+        self.states.append(s)
+        self.actions.append(a)
+        self.rewards.append(r)
+        self.dones.append(done)
+        self.states2.append(s2)
         
         self.length = self.length + 1
             
         if (self.length > self.capacity): 
-            self.states = np.delete(self.states,(0), axis = 0)
-            self.actions = np.delete(self.actions,(0), axis = 0)
-            self.rewards = np.delete(self.rewards,(0), axis = 0)
-            self.dones = np.delete(self.dones,(0), axis = 0)
-            self.states2 = np.delete(self.states2,(0), axis = 0)           
+            self.states.pop(0)
+            self.actions.pop(0)
+            self.rewards.pop(0)
+            self.dones.pop(0)
+            self.states2.pop(0)
             self.length = self.length - 1
             
         
     def sample(self,batch_size):
         if (self.length >= batch_size):
             idx = random.sample(range(0,self.length),batch_size)
-            s = self.states[idx,:]
-            a = self.actions[idx,:]
-            r = self.rewards[idx,:]
-            d = self.dones[idx,:]
-            s2 = self.states2[idx,:]
+            s = [self.states[i] for i in idx]
+            a = [self.actions[i] for i in idx]
+            r = [self.rewards[i] for i in idx]
+            d = [self.dones[i] for i in idx]
+            s2 = [self.states2[i] for i in idx]
                 
-            return list([s,a,r,s2,d])
+            return [s, a, r, s2, d]
                     
