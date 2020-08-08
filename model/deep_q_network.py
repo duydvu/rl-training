12be3d9ca0c662338338eff6f1128bd8e5dd14a4
Vũ Đức Duy <duyisking1060@gmail.com>
2020-08-08 13:22:03 +0000

    Refactor code

--- model/deep_q_network.py
+++ model/deep_q_network.py
@@ -14,7 +14,6 @@ from random import random, randrange
 
 # Deep Q Network off-policy
 class DQN: 
-   
     def __init__(
             self,
             input_max_x, #The number of inputs for the DQN network
@@ -28,115 +27,114 @@ class DQN:
             tau = 0.125, #The factor for updating the DQN target network from the DQN network
             model = None, #The DQN model
             target_model = None, #The DQN target model 
-            sess=None
-            
+            sess = None,
     ):
-      self.input_max_x = input_max_x
-      self.input_max_y = input_max_y
-      self.action_space = action_space
-      self.gamma = gamma
-      self.epsilon = epsilon
-      self.epsilon_min = epsilon_min
-      self.epsilon_decay = epsilon_decay
-      self.learning_rate = learning_rate
-      self.tau = tau
-            
-      #Creating networks
-      self.model = self.create_model() #Creating the DQN model
-      self.target_model = self.create_model() #Creating the DQN target model
-      
-      #Tensorflow GPU optimization
-      config = tf.compat.v1.ConfigProto()
-      config.gpu_options.allow_growth = True
-      self.sess = tf.compat.v1.Session(config=config)
-      K.set_session(sess)
-      self.sess.run(tf.compat.v1.global_variables_initializer()) 
+        self.input_max_x = input_max_x
+        self.input_max_y = input_max_y
+        self.action_space = action_space
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.epsilon_min = epsilon_min
+        self.epsilon_decay = epsilon_decay
+        self.learning_rate = learning_rate
+        self.tau = tau
+              
+        #Creating networks
+        self.model = self.create_model() #Creating the DQN model
+        self.target_model = self.create_model() #Creating the DQN target model
+        
+        #Tensorflow GPU optimization
+        config = tf.compat.v1.ConfigProto()
+        config.gpu_options.allow_growth = True
+        self.sess = tf.compat.v1.Session(config=config)
+        K.set_session(sess)
+        self.sess.run(tf.compat.v1.global_variables_initializer()) 
       
     def create_model(self):
-      input_view = Input(shape=(self.input_max_x, self.input_max_y, 5))
-      conv = Conv2D(filters=16, kernel_size=3, strides=1, activation='relu')(input_view)
-      max_pool = MaxPool2D(pool_size=2, strides=2)(conv)
-      conv2 = Conv2D(filters=32, kernel_size=2, activation='relu')(max_pool)
-      max_pool2 = MaxPool2D(pool_size=2, strides=1)(conv2)
-      flatten = Flatten()(max_pool2)
-      y = Dense(128, activation='relu')(flatten)
-      model1 = Model(inputs=input_view, outputs=y)
+        input_view = Input(shape=(self.input_max_x, self.input_max_y, 5))
+        conv = Conv2D(filters=16, kernel_size=3, strides=1, activation='relu')(input_view)
+        max_pool = MaxPool2D(pool_size=2, strides=2)(conv)
+        conv2 = Conv2D(filters=32, kernel_size=2, activation='relu')(max_pool)
+        max_pool2 = MaxPool2D(pool_size=2, strides=1)(conv2)
+        flatten = Flatten()(max_pool2)
+        y = Dense(128, activation='relu')(flatten)
+        model1 = Model(inputs=input_view, outputs=y)
 
-      input_energy = Input(shape=(1,))
-      model2_output = Dense(128, activation='tanh')(input_energy)
-      model2 = Model(inputs=input_energy, outputs=model2_output)
+        input_energy = Input(shape=(1,))
+        model2_output = Dense(128, activation='tanh')(input_energy)
+        model2 = Model(inputs=input_energy, outputs=model2_output)
 
-      mul = multiply([model1.output, model2.output])
+        mul = multiply([model1.output, model2.output])
 
-      output = Dense(self.action_space, activation='linear')(mul)
+        output = Dense(self.action_space, activation='linear')(mul)
 
-      model = Model(inputs=[input_view, input_energy], outputs=output)
-      adam = optimizers.adam(lr=self.learning_rate)
-      model.compile(optimizer=adam, loss='mse')
-      return model
+        model = Model(inputs=[input_view, input_energy], outputs=output)
+        adam = optimizers.adam(lr=self.learning_rate)
+        model.compile(optimizer=adam, loss='mse')
+        return model
 
 
     @staticmethod
     def transform_state(state):
-      view, players = state
-      model_view = np.zeros((view.shape[0] + 2, view.shape[1] + 2, 5), dtype=float)
-      model_view[:, :, 0] = -5
-      model_view[1:-1, 1:-1, 0] = view
-      model_view[model_view > 0] = model_view[model_view > 0] / 1000
-      for player in players:
-        if player['id'] == 1:
-          energy = player['energy'] / 50
-        x = player['x']
-        y = player['y']
-        model_view[x + 1, y + 1, player['id']] = 1
-      return [model_view, [energy]]
+        view, players = state
+        model_view = np.zeros((view.shape[0] + 2, view.shape[1] + 2, 5), dtype=float)
+        model_view[:, :, 0] = -5
+        model_view[1:-1, 1:-1, 0] = view
+        model_view[model_view > 0] = model_view[model_view > 0] / 1000
+        for player in players:
+            if player['id'] == 1:
+                energy = player['energy'] / 50
+            x = player['x']
+            y = player['y']
+            model_view[x + 1, y + 1, player['id']] = 1
+        return [model_view, [energy]]
   
     
     def act(self, state):
-      model_state = self.transform_state(state)
-      a_max = np.argmax(self.model.predict([[model_state[0]], [model_state[1]]]))      
-      if (random() < self.epsilon):
-        a_chosen = randrange(self.action_space)
-      else:
-        a_chosen = a_max      
-      return a_chosen
+        model_state = self.transform_state(state)
+        a_max = np.argmax(self.model.predict([[model_state[0]], [model_state[1]]]))      
+        if (random() < self.epsilon):
+            a_chosen = randrange(self.action_space)
+        else:
+            a_chosen = a_max      
+        return a_chosen
     
     
     def replay(self,samples,batch_size):
-      states, actions, rewards, new_states, dones = samples
-      states = list(zip(*map(self.transform_state, states)))
-      states = [list(states[0]), list(states[1])]
-      new_states = list(zip(*map(self.transform_state, new_states)))
-      new_states = [list(new_states[0]), list(new_states[1])]
-      targets = self.target_model.predict(states)
-      Q_futures = np.max(self.target_model.predict(new_states),
-                         axis=1)
+        states, actions, rewards, new_states, dones = samples
+        states = list(zip(*map(self.transform_state, states)))
+        states = [list(states[0]), list(states[1])]
+        new_states = list(zip(*map(self.transform_state, new_states)))
+        new_states = [list(new_states[0]), list(new_states[1])]
+        targets = self.target_model.predict(states)
+        Q_futures = np.max(self.target_model.predict(new_states),
+                           axis=1)
 
-      for i in range(0,batch_size):
-        action = actions[i]
-        reward = rewards[i]
-        done = dones[i]
-        if done:
-          targets[i,action] = reward # if terminated, only equals reward
-        else:
-          Q_future = Q_futures[i]
-          targets[i,action] = reward + Q_future * self.gamma
+        for i in range(0,batch_size):
+            action = actions[i]
+            reward = rewards[i]
+            done = dones[i]
+            if done:
+                targets[i,action] = reward # if terminated, only equals reward
+            else:
+                Q_future = Q_futures[i]
+                targets[i,action] = reward + Q_future * self.gamma
 
-      #Training
-      loss = self.model.train_on_batch(states, targets)
+        #Training
+        loss = self.model.train_on_batch(states, targets)
     
     def target_train(self): 
-      weights = self.model.get_weights()
-      target_weights = self.target_model.get_weights()
-      for i in range(0, len(target_weights)):
-        target_weights[i] = weights[i] * self.tau + target_weights[i] * (1 - self.tau)
-      
-      self.target_model.set_weights(target_weights) 
+        weights = self.model.get_weights()
+        target_weights = self.target_model.get_weights()
+        for i in range(0, len(target_weights)):
+            target_weights[i] = weights[i] * self.tau + target_weights[i] * (1 - self.tau)
+        
+        self.target_model.set_weights(target_weights) 
     
     
     def update_epsilon(self):
-      self.epsilon =  self.epsilon*self.epsilon_decay
-      self.epsilon =  max(self.epsilon_min, self.epsilon)
+        self.epsilon =  self.epsilon*self.epsilon_decay
+        self.epsilon =  max(self.epsilon_min, self.epsilon)
     
     
     def save_model(self, model_path):
