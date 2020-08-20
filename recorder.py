fd56d3cadf6ca4a0fcbaea870624adab16ebc8f0
Vũ Đức Duy <duyisking1060@gmail.com>
2020-08-20 15:37:35 +0000

    Update recorder

--- recorder.py
+++ recorder.py
@@ -8,11 +8,10 @@ from utils import get_now_str
 class Recorder:
     def __init__(self, header: List[str]):
         self.header = header
-        self.csv_file = open("Data/data_%s.csv" % get_now_str(), 'w')
-        pd.DataFrame(columns=self.header).to_csv(
-            self.csv_file, encoding='utf-8', index=False, header=True)
+        self.csv_file = "Data/data_%s.csv" % get_now_str()
+        with open(self.csv_file, 'w') as f:
+            pd.DataFrame(columns=self.header).to_csv(f, index=False, header=True)
     
     def append(self, data: List):
-        data = np.hstack(data).reshape((1, len(data)))
-        pd.DataFrame(data).to_csv(
-            self.csv_file, encoding='utf-8', index=False, header=False)
+        with open(self.csv_file, 'a') as f:
+            pd.DataFrame([data]).to_csv(f, index=False, header=False)
