983b4d7845ff2195862554fb544dc80e06da7528
Vũ Đức Duy <duyisking1060@gmail.com>
2020-09-01 03:29:58 +0000

    Update predictors

--- predict/ray_tf_v2.py
+++ predict/ray_tf_v2.py
@@ -4,38 +4,70 @@ from ray.rllib.models import ModelCatalog
 import numpy as np
 
 
-from MultiAgentGymMinerEnv import MultiAgentGymMinerEnv
-from model.ray_tf_model import RayTFModel
+from MultiStepsMultiAgentGymMinerEnv import MultiStepsMultiAgentGymMinerEnv
+from model.ray_tf_model_v2 import RayTFModel
 
 
 class RayTFPredictor():
     def __init__(self):
         ModelCatalog.register_custom_model("my_model", RayTFModel)
         ray.init()
-        single_env = MultiAgentGymMinerEnv({})
+        self.max_step = 5
+        single_env = MultiStepsMultiAgentGymMinerEnv({'max_step': self.max_step})
         obs_space = single_env.observation_space
         act_space = single_env.action_space
         self.config = apex.APEX_DEFAULT_CONFIG.copy()
-        self.config['num_workers'] = 0
-        self.config['in_evaluation'] = True
-        self.config['multiagent'] = {
-            'policies': {
-                'dqn1': (None, obs_space, act_space, {
-                    'model': {
-                        'custom_model': 'my_model'
-                    },
-                    'gamma': 0.99
-                })
+        self.config = {
+            **self.config,
+            'num_workers': 0,
+            'in_evaluation': True,
+            'multiagent': {
+                'policies': {
+                    'dqn1': (None, obs_space, act_space, {
+                        'model': {
+                            'custom_model': 'my_model',
+                            'custom_model_config': {
+                                'max_step': 5,
+                                'embedding_size': 8,
+                                'conv1_filters': [
+                                    (32, 4, 1),
+                                    (64, 2, 2),
+                                    (128, 2, 2),
+                                ],
+                                'conv2_filter': (128, 2, 1)
+                            }
+                        },
+                        'gamma': 0.99
+                    })
+                },
+                'policy_mapping_fn': lambda agent_id: 'dqn1',
             },
-            'policy_mapping_fn': lambda agent_id: 'dqn1',
+            'env_config': {
+                'max_step': self.max_step,
+            }
         }
-        self.agent = apex.ApexTrainer(env=MultiAgentGymMinerEnv, config=self.config)
-        self.agent.restore('checkpoints/checkpoint_81/checkpoint-81')
+        self.agent = apex.ApexTrainer(
+            env=MultiStepsMultiAgentGymMinerEnv, config=self.config)
+        self.agent.restore('checkpoints/checkpoint_881/checkpoint-881')
+        self.step_states = []
+    
+    def reset(self):
+        self.step_states = []
 
     def compute_action(self, state):
-        state = self.preprocess(state)
-        return self.agent.compute_action(state, policy_id='dqn1', explore=False)
-    
+        step_state = self.preprocess(state)
+        if len(self.step_states) == 0:
+            self.step_states = (
+                [step_state[0] for _ in range(self.max_step)],
+                [step_state[1] for _ in range(self.max_step)],
+            )
+        else:
+            self.step_states = (
+                self.step_states[0][1:] + [step_state[0]],
+                self.step_states[1][1:] + [step_state[1]],
+            )
+        return self.agent.compute_action(self.step_states, policy_id='dqn1', explore=False)
+
     def preprocess(self, state):
         # Building the map
         view = np.zeros([21, 9, 6], dtype=float)
