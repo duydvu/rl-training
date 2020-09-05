ac25f00ad9237375ea17f588474737a5d30a0959
Vũ Đức Duy <duyisking1060@gmail.com>
2020-09-05 09:07:43 +0000

    Update web

--- web.py
+++ web.py
@@ -6,7 +6,9 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 from random import randint
 
-from MinerEnv import MinerEnv
+from src.game.miner_env import MinerEnv
+from src.policies.bot1 import Bot1
+from src.policies.bot2 import Bot2
 
 simplefilter(action='ignore', category=FutureWarning)
 
@@ -26,6 +28,10 @@ state = miner_env.state
 s = None
 playerId = 1
 step = 0
+bot1 = Bot1(2)
+bot2 = Bot1(3)
+bot3 = Bot2(4)
+
 
 @app.route('/next', methods=['GET'])
 def get_next():
@@ -34,9 +40,9 @@ def get_next():
         action = predictor.compute_action(s)
         miner_env.step({
             '1': action,
-            '2': randint(0, 5),
-            '3': randint(0, 5),
-            '4': randint(0, 5),
+            '2': bot1.compute_action(s),
+            '3': bot2.compute_action(s),
+            '4': bot3.compute_action(s),
         })
         s = miner_env.get_state()
         step += 1
@@ -58,10 +64,14 @@ def reset():
     map_id = int(body.get('map_id', 1))
     x = int(body.get('init_x', 0))
     y = int(body.get('init_y', 0))
-    miner_env.send_map_info(map_id, x, y)
+    miner_env.send_map_info(map_id, x, y, number_of_players=4)
     miner_env.reset()
     s = miner_env.get_state()
     step = 0
+    predictor.reset()
+    bot1.reset()
+    bot2.reset()
+    bot3.reset()
     return jsonify({
         'state': miner_env.get_readable_state(),
         'status': status_map[state.players[playerId]['status']],
